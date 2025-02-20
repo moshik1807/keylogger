9730a36b7ae71aa4826dc9d23fea8cd44d436ffa
Yakir Uzan <yakiruzangreen@gmail.com>
2025-02-20 15:31:27 +0200

    Saves data to a file

--- fileWriter.py
+++ fileWriter.py
@@ -4,3 +4,7 @@ class IWriter(ABC):
     @abstractmethod
     def send_data(self, data: str, machine_name: str) -> None:
         pass
+
+class FileWriter(IWriter):
+    def __init__(self, file_path: str):
+        self.file_path = file_path
