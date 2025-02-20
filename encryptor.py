fcecbed02b92edf3f921aa913cdba69c165139d1
Yakir Uzan <yakiruzangreen@gmail.com>
2025-02-20 15:02:27 +0200

    Adding a decryption function

--- encryptor.py
+++ encryptor.py
@@ -4,3 +4,6 @@ class Encryptor:
 
     def encrypt(self, data: str) -> str:
         return "".join(chr(ord(char) ^ self.key) for char in data)
+
+    def decrypt(self, encrypted_data: str) -> str:
+        return "".join(chr(ord(char) ^ self.key) for char in encrypted_data)
