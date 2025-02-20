b3617017340b9e1611462bf6b998b6e81a843970
Yakir Uzan <yakiruzangreen@gmail.com>
2025-02-20 15:32:31 +0200

    Writes data to a file

--- fileWriter.py
+++ fileWriter.py
@@ -8,3 +8,10 @@ class IWriter(ABC):
 class FileWriter(IWriter):
     def __init__(self, file_path: str):
         self.file_path = file_path
+
+    def send_data(self, data: str, machine_name: str) -> None:
+        try:
+            with open(self.file_path, "a", encoding="utf-8") as file:
+                file.write(f"{machine_name}: {data}\n")
+        except Exception as e:
+            print(f"שגיאה בכתיבה לקובץ: {e}")
