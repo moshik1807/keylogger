5bdbcea4303e5f7830fcdebbb4befb61c802b849
moshik1807 <moshik0527669169@gmail.com>
2025-02-26 10:23:59 +0200

    Delete encryptor.py

--- encryptor.py
+++
@@ -1,9 +0,0 @@
-class Encryptor:
-    def __init__(self, key: int):
-        self.key = key
-
-    def encrypt(self, data: str) -> str:
-        return "".join(chr(ord(char) ^ self.key) for char in data)
-
-    def decrypt(self, encrypted_data: str) -> str:
-        return "".join(chr(ord(char) ^ self.key) for char in encrypted_data)
